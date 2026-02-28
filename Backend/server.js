48e6018d4ff9d26a24951ece8a8251dabc8095ba
SakshamSinghal20 <sakshamsinghal2020@gmail.com>
2026-02-28 11:18:14 +0530

    connection made via socket io

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
+
 server.listen(5000, () => {
     console.log("server running on port 5000")
+})
+
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        orgin: "https://localhost:5173",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("user connected", socket.id);
 })
