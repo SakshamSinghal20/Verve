c8ee8caa3da250816166e7ca79adbc0501eb5a18
SakshamSinghal20 <sakshamsinghal2020@gmail.com>
2026-03-02 15:46:42 +0530

    room logic implemented - when a person clickes on join it shows user joined the room with room id

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,11 @@ const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:5173",
+        method: ["GET", "POST"],
     },
+});
+app.get("/", (req, res) => {
+    res.send("Server is running");
 });
 
 io.on("connection", (socket) => {
